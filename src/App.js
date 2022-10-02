e8c49a3862ac663ae83c1c32546b79bf3ff12ab9
Nicholas <nicholas.dass@gmail.com>
2022-10-01 23:48:03 -0400

    SmartBrain Front-End Components

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ParticlesBg type="circle" bg={true} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*
+            
+            <FaceRecognition />*/}
+
+
+    </div>
+  );
+}
+
+export default App;
